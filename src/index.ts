f565928ef67a5622409866abaf206225a6aa10a7
Florian Mäder <florian@maederbiel.ch>
2019-10-25 23:29:59 +0200

    fix(package): add entrypoint

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { QueueError, QueueErrorInput } from './error';
+import { QueueStore, QueueStoreData } from './store';
+import { Queue } from './queue';
+
+export {
+	QueueError,
+	QueueErrorInput,
+	QueueStore,
+	QueueStoreData,
+	Queue,
+}
